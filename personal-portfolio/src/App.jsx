741a99391ef3f51546958b39f569584c7002c389
jesualujan <lujan.oficial@gmail.com>
2024-02-12 01:09:13 -0600

    ✨ importamos el tema

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -1,11 +1,22 @@
 import './main.css'
+import styled, {ThemeProvider} from 'styled-components'
+import { darkTheme } from './utils/Theme'
+
+const Body = styled.div`
+  background-color: ${({theme}) => theme.bg };
+  width: 100%;
+  overflow-x: hidden;
+`;
+
 
 function App() {
 
   return (
-    <>
-      <h1>funciona</h1>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <Body>
+      Portfolio
+      </Body>
+    </ThemeProvider>
   )
 }
 
